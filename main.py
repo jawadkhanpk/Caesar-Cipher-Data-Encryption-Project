8230c51b4c8b4abb02d578768764de8f5c982c4c
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-16 22:48:09 +0500

    another commit

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type Encode to encrypt, Type Decode to decrypt")
-text = input("Type you message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+from art import logo
+print(logo)
+
+def caeser(start_text, shift_amount, cipher_direction):
+    end_text = ""
+
+    if cipher_direction == "decode":
+        shift_amount *= -1
+        # let shift amount = 5 then it will become -5 in this case
+
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+
+    print(f"The {cipher_direction}d text is {end_text}")
 
 
+
+"""
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
     for letter in plain_text:
@@ -24,7 +42,24 @@ def decrypt(cipher_text, shift_amount):
         plain_text += alphabet[new_position]
     print(f"The decoded text is {plain_text}")
 
+
+
 if direction == "encrypt":
     encrypt(plain_text=text,shift_amount=shift)
 elif direction == "decrypt":
     decrypt(cipher_text= text, shift_amount=shift)
+
+"""
+
+should_continue = True
+while should_continue:
+    direction = input("Type Encode to encrypt, Type Decode to decrypt:\n")
+    text = input("Type you message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    caeser(start_text= text, shift_amount= shift, cipher_direction=direction)
+
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if result == "no":
+        should_continue = False
+        print("Good Bye!")
