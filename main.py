c56dc877cea68e377602dd5bb27a96f48b3eb639
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-16 21:43:02 +0500

    another commit

--- main.py
+++ main.py
@@ -17,7 +17,14 @@ def encrypt(plain_text, shift_amount):
 
 
 def decrypt(cipher_text, shift_amount):
-    
+    plain_text = ""
+    for letter in cipher_text:
+        position = alphabet.index((letter))
+        new_position = position - shift_amount
+        plain_text += alphabet[new_position]
+    print(f"The decoded text is {plain_text}")
 
-
-encrypt(plain_text=text,shift_amount=shift)
+if direction == "encrypt":
+    encrypt(plain_text=text,shift_amount=shift)
+elif direction == "decrypt":
+    decrypt(cipher_text= text, shift_amount=shift)
